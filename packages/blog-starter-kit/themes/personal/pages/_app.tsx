6d78837ddf53c49600ab9fc2b57ba3a76682d745
AdelodunAbraham <ab.adelodun@gmail.com>
2026-01-07 00:35:56 +0100

    feat: update Sora font weights to include additional styles for improved typography

--- packages/blog-starter-kit/themes/personal/pages/_app.tsx
+++ packages/blog-starter-kit/themes/personal/pages/_app.tsx
@@ -4,7 +4,7 @@ import { Poppins, Sora } from 'next/font/google';
 import { useEffect } from 'react';
 import '../styles/index.css';
 
-const sora = Sora({ subsets: ['latin'], variable: '--font-sora', weight: ['400', '500', '600', '700', '800'] });
+const sora = Sora({ subsets: ['latin'], variable: '--font-sora', weight: ['100', '200', '300', '400', '500', '600', '700', '800'] });
 const poppins = Poppins({
 	subsets: ['latin'],
 	variable: '--font-poppins',
